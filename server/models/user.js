02e7d4df336ec292b51759ce90d36324bf23edc9
nandanaabhay <nandanaabhay@gmail.com>
2025-03-05 00:17:31 +0530

    Added server models

---
+++ server/models/user.js
@@ -0,0 +1,111 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 20,
+      unique: true,
+    },
+    device_id: {
+      type: String,
+      required: false,
+      default: "NA",
+    },
+    clerk_id: {
+      type: String,
+      required: false,
+      default: "NA",
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      max: 50,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 8,
+    },
+    aadhaar_number: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    parent_email: {
+      type: String,
+      required: false,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    imageUrl: {
+      type: String,
+      default: "",
+    },
+    age_verified: {
+      type: Boolean,
+      default: false,
+    },
+    is_active: {
+      type: Boolean,
+      default: true,
+    },
+    is_online: {
+      type: Boolean,
+      default: false,
+    },
+    is_flagged: {
+      type: Boolean,
+      default: false,
+    },
+    flag_count: {
+      type: Number,
+      default: 0,
+    },
+    last_active: {
+      type: Date,
+      default: Date.now,
+    },
+    created_at: {
+      type: Date,
+      default: Date.now,
+    },
+    updated_at: {
+      type: Date,
+      default: Date.now,
+    },
+    isAvatarImageSet: {
+      type: Boolean,
+      default: false,
+    },
+    avatarImage: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" }, // Auto-manages created_at & updated_at
+  }
+);
+
+const User = mongoose.model("Users", userSchema);
+
+export default User;

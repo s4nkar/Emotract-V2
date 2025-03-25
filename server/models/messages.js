cf7a92d697b2c512a6217b8a8bc06dbd6b9fc8b7
GowriVenugopal <gowrivenugopal04@gmail.com>
2025-03-25 19:35:55 +0530

    Added new model files

---
+++ server/models/messages.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const MessageSchema = mongoose.Schema(
+  {
+    chat_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Chats",
+      required: true,
+    },
+    sender_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users",
+      required: true,
+    },
+    text: { type: String, required: true },
+    sent_at: {
+      type: Date,
+      default: Date.now,
+    },
+    read_by: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "Users" }
+    ],
+    is_active: {
+      type: Boolean,
+      default: true,
+    },
+    processing_status: {
+      type: String,
+      default: 'processing',
+    },
+    reaction: {
+      emoji: { type: String },
+      reacted_by: [
+        { type: mongoose.Schema.Types.ObjectId, ref: "Users" }
+      ],
+      reacted_at: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Messages", MessageSchema);

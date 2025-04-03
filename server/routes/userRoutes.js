4842a9467f0f85861348d7fdb7a4efcc12242dd9
sankar <sankusanku001@gmail.com>
2025-04-03 17:36:49 +0100

    user controller bug fix

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,15 +1,12 @@
 
-import { updateProfile ,logoutUser,} from "../controllers/usercontroller.js";
-
-import { register, searchContact } from '../controllers/userController.js';
-import { login } from '../controllers/userController.js';
-
+import { updateProfile ,logoutUser, login, searchContact, register,} from "../controllers/usercontroller.js";
 const router = express.Router();
 
 // Route for User Registration
 router.post('/register', register);
 router.post('/login', login);
 router.get('/search-contact', searchContact);
+
 // Update user profile
 router.put("/update-profile/:id", updateProfile);
 router.post('/logout/:id',logoutUser);
